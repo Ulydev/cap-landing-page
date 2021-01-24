86b6a92b106af17af55d18432d9072bfe06aed24
ulydev <ulydev@live.fr>
2021-01-24 19:50:48 +0100

    add dots pattern and update main layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,14 +6,17 @@ import Hero from "../components/Hero"
 import Whitepaper from "../components/Whitepaper"
 
 const Index = () => (
-  <div className="mx-8 my-8 md:max-w-3xl md:mx-auto md:my-16 text-white">
-    <Header />
+  <div className="relative">
+    <div className="absolute w-full h-full pattern-dots text-primary-100 opacity-10" />
+    <div className="relative z-10 mx-8 py-8 md:max-w-3xl md:mx-auto text-white">
+      <Header />
 
-    <Hero />
-    <Features />
-    <Whitepaper />
+      <Hero />
+      <Features />
+      <Whitepaper />
 
-    <Footer />
+      <Footer />
+    </div>
   </div>
 )
 
