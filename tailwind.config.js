0fbcd45c9c5642aa59d1877eb65be65d36efb0af
ulydev <ulydev@live.fr>
2021-01-24 19:49:44 +0100

    update tailwind palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require("tailwindcss/colors")
+
 module.exports = {
   future: {
     removeDeprecatedGapUtilities: true,
@@ -7,9 +9,23 @@ module.exports = {
     content: ["./src/**/*.html", "./src/**/*.tsx"],
   },
   theme: {
-    extend: {
-      colors: {
-        primary: "#46e850",
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      gray: colors.warmGray,
+      white: colors.white,
+      primary: {
+        100: "#43ff44",
+        200: "#39dc38",
+        300: "#2fba2c",
+        400: "#249820",
+      },
+      secondary: {
+        100: "#c5c5c7",
+        200: "#25254f",
+        300: "#1e1e44",
+        400: "#1d1c3d",
       },
     },
   },
